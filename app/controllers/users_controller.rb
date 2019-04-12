45da737b1650f9a6b125b2ddb75d946fe8ce1ff6
Nick Killin <nicholaskillin@gmail.com>
2019-04-11 22:37:04 -0400

    added users controller and new user form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,29 @@
 class UsersController < ApplicationController
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      flash[:success] = "Account created successfully."
+      redirect_to root_url
+    else
+      flash[:danger] = "Your account was not created. Please correct the errors below and try again."
+    end
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
 end
