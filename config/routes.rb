1b88e1fc5ccf2ca2007481fd88e2150a360a7b14
Nick Killin <nicholaskillin@gmail.com>
2019-04-17 22:44:03 -0400

    added login form

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   
+  get 'sessions/new'
   root 'static_pages#home'
-  resources  :users
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
-  
+  get '/login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  resources  :users
+  resources  :password_resets, only: [:new, :create, :edit, :update]
 end
