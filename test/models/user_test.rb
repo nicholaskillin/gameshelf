376a0091895d16eec32bf1a64161ef9fc4a746ee
Nick Killin <nicholaskillin@gmail.com>
2019-04-21 14:03:19 -0400

    add test to verify user email downcase

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com", password: "123456")
   end
   
   test "email addresses should be unique" do
@@ -11,6 +11,12 @@ class UserTest < ActiveSupport::TestCase
     duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
+  end
+
+  test "email should always downcase" do
+    user = User.new(name: "Example User", email: "ALLCAPS@example.com", password: "123456")
+    user.save
+    assert_equal user.email, "allcaps@example.com"
   end
 
 end
