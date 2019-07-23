6b7e237bbc76ad792a4b5a6c3331dc0f200f09ca
Nick Killin <nicholaskillin@gmail.com>
2019-07-23 08:02:40 -0400

    fixed category game join table

--- db/migrate/20190723012620_create_join_table_category_game.rb
+++ db/migrate/20190723012620_create_join_table_category_game.rb
@@ -1,6 +1,6 @@
 class CreateJoinTableCategoryGame < ActiveRecord::Migration[5.2]
   def change
-    create_join_table :Categories, :Games do |t|
+    create_join_table :categories, :games do |t|
       t.index [:category_id, :game_id]
       t.index [:game_id, :category_id]
     end
--- db/schema.rb
+++ db/schema.rb
@@ -12,18 +12,18 @@
 
 ActiveRecord::Schema.define(version: 2019_07_23_012620) do
 
-  create_table "Categories_Games", id: false, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "Category_id", null: false
-    t.bigint "Game_id", null: false
-    t.index ["Category_id", "Game_id"], name: "index_Categories_Games_on_category_id_and_game_id"
-    t.index ["Game_id", "Category_id"], name: "index_Categories_Games_on_game_id_and_category_id"
-  end
-
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "bgg_id"
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "categories_games", id: false, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "category_id", null: false
+    t.bigint "game_id", null: false
+    t.index ["category_id", "game_id"], name: "index_categories_games_on_category_id_and_game_id"
+    t.index ["game_id", "category_id"], name: "index_categories_games_on_game_id_and_category_id"
   end
 
   create_table "games", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
