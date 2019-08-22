8491a49b4cc5c92144ef2ddf7113582546f85db5
Nick Killin <nicholaskillin@gmail.com>
2019-08-21 21:50:12 -0400

    added redirect to login if not logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :require_login
+
   include SessionsHelper
+
+  private
+
+  def require_login
+    unless current_user
+      redirect_to login_url
+    end
+  end
 end
