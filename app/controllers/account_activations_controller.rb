2f187935ba049bc2e4ad5847093c9427c1d708c0
Nick Killin <nicholaskillin@gmail.com>
2019-08-21 21:55:26 -0400

    fixed user setup flow

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,4 +1,6 @@
 class AccountActivationsController < ApplicationController
+  skip_before_action :require_login
+
   def edit
     user = User.find_by(email: params[:email])
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
