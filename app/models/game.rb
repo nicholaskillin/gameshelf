15301c0257bc2f17384820dfa3437b1d536bc1ae
Nick Killin <nicholaskillin@gmail.com>
2019-09-17 13:59:08 -0400

    destroy associations directly

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,6 @@
 class Game < ApplicationRecord
-  has_many :game_users
-  has_many :users, through: :game_users, dependent: :destroy
+  has_many :game_users, dependent: :destroy
+  has_many :users, through: :game_users
   has_and_belongs_to_many :categories
   has_and_belongs_to_many :mechanics
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   attr_accessor :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
-  has_many :game_users
-  has_many :games, through: :game_users, dependent: :destroy
+  has_many :game_users, dependent: :destroy
+  has_many :games, through: :game_users
 
   has_secure_password
 
