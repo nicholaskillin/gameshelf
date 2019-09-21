71bb6e7b280d87675f89133ee66acfbb9286cd7b
Nick Killin <nicholaskillin@gmail.com>
2019-09-21 13:43:27 -0400

    trying to fix heroku precompile error

--- app/assets/javascripts/game_details_modal.js
+++ app/assets/javascripts/game_details_modal.js
@@ -1,3 +1,5 @@
+'use strict';
+
 $( document ).ready(function() {
   $('#gameDetailsModal').on('show.bs.modal', function (event) {
     var button = $(event.relatedTarget) // Button that triggered the modal
@@ -19,25 +21,25 @@ $( document ).ready(function() {
       dataType: 'json',
       data: { id: gameID },
       success: function (response) {
-        let game = response.game
-        console.log(response);
-        modal.find('#gameDataTitle').text(game.title)
-        modal.find('#gameDetailsArtwork').attr("src", game.image)
-        cleanDesc = game.description.replace('&#10;', "<br>");
-        modal.find('#gameDetailsDescription').append(cleanDesc)
+        // let game = response.game
+        // console.log(response);
+        // modal.find('#gameDataTitle').text(game.title)
+        // modal.find('#gameDetailsArtwork').attr("src", game.image)
+        // cleanDesc = game.description.replace('&#10;', "<br>");
+        // modal.find('#gameDetailsDescription').append(cleanDesc)
         
-        // Adds player count to game details modal
-        modal.find('#gameDataPlayerCount').html('<strong>Players:</strong>' + " " + game.min_players)
-        if (game.min_players != game.max_players) {
-          modal.find('#gameDataPlayerCount').append(" - " + game.max_players)
-        }
+        // // Adds player count to game details modal
+        // modal.find('#gameDataPlayerCount').html('<strong>Players:</strong>' + " " + game.min_players)
+        // if (game.min_players != game.max_players) {
+        //   modal.find('#gameDataPlayerCount').append(" - " + game.max_players)
+        // }
         
-        // Adds play time to game details modal
-        modal.find('#gameDataPlayTime').html('<strong>Play Time:</strong>' + " " + game.min_play_time)
-        if (game.min_play_time != game.max_play_time) {
-          modal.find('#gameDataPlayTime').append(" - " + game.max_play_time)
-        }
-        modal.find('#gameDataPlayTime').append(" min.")
+        // // Adds play time to game details modal
+        // modal.find('#gameDataPlayTime').html('<strong>Play Time:</strong>' + " " + game.min_play_time)
+        // if (game.min_play_time != game.max_play_time) {
+        //   modal.find('#gameDataPlayTime').append(" - " + game.max_play_time)
+        // }
+        // modal.find('#gameDataPlayTime').append(" min.")
       }
     });
   })
