b2aa7ad350dd709fafff03aa41ddfb135cbab8c5
Nick Killin <nicholaskillin@gmail.com>
2019-09-24 06:41:01 -0400

    the search continues

--- app/assets/javascripts/game_details_modal.js
+++ app/assets/javascripts/game_details_modal.js
@@ -14,25 +14,25 @@ $(document).ready(function () {
       type: 'GET',
       dataType: 'json',
       data: { id: gameID },
-      success: function (response) {
-        modal.find('#gameDataTitle').text(response.game.title);
-        modal.find('#gameDetailsArtwork').attr('src', response.game.image);
-        const cleanDesc = response.game.description.replace('&#10;', '<br>');
-        modal.find('#gameDetailsDescription').append(cleanDesc);
+      // success: function (response) {
+      //   modal.find('#gameDataTitle').text(response.game.title);
+      //   modal.find('#gameDetailsArtwork').attr('src', response.game.image);
+      //   const cleanDesc = response.game.description.replace('&#10;', '<br>');
+      //   modal.find('#gameDetailsDescription').append(cleanDesc);
 
-        // Adds player count to game details modal
-        modal.find('#gameDataPlayerCount').html(`<strong>Players:</strong> ${response.game.min_players}`);
-        if (response.game.min_players !== response.game.max_players) {
-          modal.find('#gameDataPlayerCount').append(` - ${response.game.max_players}`);
-        }
+      //   // Adds player count to game details modal
+      //   modal.find('#gameDataPlayerCount').html(`<strong>Players:</strong> ${response.game.min_players}`);
+      //   if (response.game.min_players !== response.game.max_players) {
+      //     modal.find('#gameDataPlayerCount').append(` - ${response.game.max_players}`);
+      //   }
 
-        // Adds play time to game details modal
-        modal.find('#gameDataPlayTime').html(`<strong>Play Time:</strong> ${response.game.min_play_time}`);
-        if (response.game.min_play_time !== response.game.max_play_time) {
-          modal.find('#gameDataPlayTime').append(` - ${response.game.max_play_time}`);
-        }
-        modal.find('#gameDataPlayTime').append(' min.');
-      },
+      //   // Adds play time to game details modal
+      //   modal.find('#gameDataPlayTime').html(`<strong>Play Time:</strong> ${response.game.min_play_time}`);
+      //   if (response.game.min_play_time !== response.game.max_play_time) {
+      //     modal.find('#gameDataPlayTime').append(` - ${response.game.max_play_time}`);
+      //   }
+      //   modal.find('#gameDataPlayTime').append(' min.');
+      // },
     });
   });
 });
