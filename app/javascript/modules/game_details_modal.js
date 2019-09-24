c81f80a3939a901f978ca7481884565123717236
Nick Killin <nicholaskillin@gmail.com>
2019-09-24 09:43:59 -0400

    adding game details modal back in

--- app/javascript/modules/game_details_modal.js
+++ app/javascript/modules/game_details_modal.js
@@ -3,42 +3,38 @@ export default { gameDetails: true }
 $(document).ready(function () {
   $('#gameDetailsModal').on('show.bs.modal', function (event) {
     const button = $(event.relatedTarget); // Button that triggered the modal
-    // const gameID = button.attr('id'); // Extract info from data-* attributes
-    // const modal = $(this);
+    const gameID = button.attr('id'); // Extract info from data-* attributes
+    const modal = $(this);
 
-    console.log(button);
+    Get game data
+    $.ajaxSetup({
+      headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') },
+    });
 
-    // Get game data
-    // $.ajaxSetup({
-    //   headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') },
-    // });
+    $.ajax({
+      url: '/games/details',
+      type: 'GET',
+      dataType: 'json',
+      data: { id: gameID },
+      success: function (response) {
+        modal.find('#gameDataTitle').text(response.game.title);
+        modal.find('#gameDetailsArtwork').attr('src', response.game.image);
+        const cleanDesc = response.game.description.replace('&#10;', '<br>');
+        modal.find('#gameDetailsDescription').append(cleanDesc);
 
-    // $.ajax({
-    //   url: '/games/details',
-    //   type: 'GET',
-    //   dataType: 'json',
-    //   data: { id: gameID },
-      // success: function (response) {
-      //   modal.find('#gameDataTitle').text(response.game.title);
-      //   modal.find('#gameDetailsArtwork').attr('src', response.game.image);
-      //   const cleanDesc = response.game.description.replace('&#10;', '<br>');
-      //   modal.find('#gameDetailsDescription').append(cleanDesc);
+        // Adds player count to game details modal
+        modal.find('#gameDataPlayerCount').html(`<strong>Players:</strong> ${response.game.min_players}`);
+        if (response.game.min_players !== response.game.max_players) {
+          modal.find('#gameDataPlayerCount').append(` - ${response.game.max_players}`);
+        }
 
-      //   // Adds player count to game details modal
-      //   modal.find('#gameDataPlayerCount').html(`<strong>Players:</strong> ${response.game.min_players}`);
-      //   if (response.game.min_players !== response.game.max_players) {
-      //     modal.find('#gameDataPlayerCount').append(` - ${response.game.max_players}`);
-      //   }
-
-      //   // Adds play time to game details modal
-      //   modal.find('#gameDataPlayTime').html(`<strong>Play Time:</strong> ${response.game.min_play_time}`);
-      //   if (response.game.min_play_time !== response.game.max_play_time) {
-      //     modal.find('#gameDataPlayTime').append(` - ${response.game.max_play_time}`);
-      //   }
-      //   modal.find('#gameDataPlayTime').append(' min.');
-      // },
-    // });
+        // Adds play time to game details modal
+        modal.find('#gameDataPlayTime').html(`<strong>Play Time:</strong> ${response.game.min_play_time}`);
+        if (response.game.min_play_time !== response.game.max_play_time) {
+          modal.find('#gameDataPlayTime').append(` - ${response.game.max_play_time}`);
+        }
+        modal.find('#gameDataPlayTime').append(' min.');
+      },
+    });
   });
 });
-
-console.log('Still working');
