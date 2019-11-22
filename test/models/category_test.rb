62d527926384294e1e510776445aae2d299b1277
Nick Killin <nicholaskillin@gmail.com>
2019-11-21 21:32:00 -0500

    added a category test

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "can create categories" do
+    category = Category.new
+    assert category.save
+  end
+
 end
