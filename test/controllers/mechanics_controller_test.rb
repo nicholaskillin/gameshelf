9af3a87bfb4105a235ae4ff9e8dff81f2bbde747
Nick Killin <nicholaskillin@gmail.com>
2019-11-21 22:16:26 -0500

    removed mechanics controller test

--- test/controllers/mechanics_controller_test.rb
+++ test/controllers/mechanics_controller_test.rb
@@ -1,9 +1,5 @@
 require 'test_helper'
 
 class MechanicsControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get mechanics_create_url
-    assert_response :success
-  end
 
 end
