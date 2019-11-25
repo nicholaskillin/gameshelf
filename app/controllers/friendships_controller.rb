69ed2e7301cb3d1835c47c14a3499e58309925d1
Nick Killin <nicholaskillin@gmail.com>
2019-11-24 22:05:00 -0500

    basic friendship index working

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,6 +1,9 @@
 class FriendshipsController < ApplicationController
   
   def index
+    @user = User.find_by_username(params[:user_username])
+    @pending_friendships = current_user.pending_friends
+    @active_friendships = current_user.active_friends
   end
-  
+
 end
