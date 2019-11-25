595e949bc6e32a0751dcb16d8de41e1f821efea0
Nick Killin <nicholaskillin@gmail.com>
2019-11-24 22:27:48 -0500

    list active and pending friends

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,8 +2,8 @@ class FriendshipsController < ApplicationController
   
   def index
     @user = User.find_by_username(params[:user_username])
-    @pending_friendships = current_user.pending_friends
-    @active_friendships = current_user.active_friends
+    @pending_friendships = @user.pending_friends
+    @active_friendships = @user.active_friends
   end
 
 end
