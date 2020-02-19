8afc03e02c7479ef73db97e5de716cd546441a22
Nick Killin <nicholaskillin@gmail.com>
2020-02-18 21:43:20 -0500

    Send availability data to game component

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -2,15 +2,14 @@ class Api::V1::GamesController < ApplicationController
 
   def index
     @user = User.find_by_username(params[:username])
-    games = @user.games.order(:title)
-    games.each |game| do
-      binding.pry
-      game_id = game.game.id
-      game_record = @user.game_users.find(game_id)
+    @games = @user.games.order(:title)
+    games = []
+    
+    @games.each do |game|
+      games.push({ game: game, availablity: game.available?(@user, game) })
     end
-    render json: { games: @user.games.order(:title),
-                   availability: @user.game_users,
-                 }
+    
+    render json: { games: games }
   end
 
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -3,4 +3,8 @@ class Game < ApplicationRecord
   has_many :users, through: :game_users
   has_and_belongs_to_many :categories
   has_and_belongs_to_many :mechanics
+
+  def available?(user, game)
+    user.game_users.find_by_game_id(game.id).available
+  end
 end
