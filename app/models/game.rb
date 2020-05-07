0d08e87623a3ccf1e025385b5c49dbb911a9d4e9
Nick Killin <nicholaskillin@gmail.com>
2020-05-07 08:55:57 -0400

    fixed default sort

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,4 +3,6 @@ class Game < ApplicationRecord
   has_many :users, through: :game_users
   has_and_belongs_to_many :categories
   has_and_belongs_to_many :mechanics
+
+  default_scope { order(title: :asc)}
 end
