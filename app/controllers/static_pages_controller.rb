cdd6987b744905deb1db85455a4ad15b36d1086c
Nick Killin <nicholaskillin@gmail.com>
2020-07-07 17:49:50 -0400

    added home page and fixed behavior when logged in

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,9 @@
 class StaticPagesController < ApplicationController
-
+  skip_before_action :require_login
   def home
+    if logged_in?
+      redirect_to user_games_url(current_user.username)
+    end
   end
 
 end
