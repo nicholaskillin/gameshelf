35c318f939a3923987064ae3fa432ede917f1a4d
Nick Killin <nicholaskillin@gmail.com>
2021-02-10 18:08:50 -0500

    pass activation url in

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,6 +7,7 @@ class UserMailer < ApplicationMailer
   #
   def account_activation(user)
     @user = user
+    @activation_url = edit_account_activation_url(@user.activation_token, email: @user.email)
     mail to: user.email, subject: "Account activation"
   end
 
