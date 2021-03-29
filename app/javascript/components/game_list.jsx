6ec01d51fcbc5a888cc0c44cbf2ae415e36a1823
Nick Killin <nicholaskillin@gmail.com>
2021-03-29 08:16:22 -0400

    Prettier

--- app/javascript/components/game_list.jsx
+++ app/javascript/components/game_list.jsx
@@ -6,8 +6,8 @@ export default function GameList({
   games: initialGameData,
   user,
 }) {
-  console.log(initialGameData)
   const [games, setGames] = useState(initialGameData)
+
   const handleSort = () => {
     const sortCriteria = $('#gameSort').val()
     function propComparator(prop) {
@@ -47,7 +47,7 @@ export default function GameList({
           </select>
         </div>
       </div>
-      <div className='row'>
+        <div className="row">
         {games.map((game) => (
           <GameCard
             currentUser={currentUser}
@@ -67,38 +67,38 @@ function GameCard({ currentUser, game, user }) {
     return currentUser !== null && currentUser.id === user.id
   }
   return (
-    <div className='col-sm-3'>
-      <div className='card game-entry'>
+    <div className="col-sm-3">
+      <div className="card game-entry">
         <img
-          className='card-img-top'
+          className="card-img-top"
           src={game.image}
           alt={`${game.title} Artwork`}
         />
-        <div className='card-body'>
-          <h5 className='card-title'>{game.title}</h5>
-          <ul className='list-group list-group-flush'>
-            <li className='list-group-item'>{`Players: ${game.min_players} - ${game.max_players}`}</li>
-            <li className='list-group-item'>{`Play Time: ${game.min_play_time} - ${game.max_play_time} min`}</li>
+        <div className="card-body">
+          <h5 className="card-title">{game.title}</h5>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">{`Players: ${game.min_players} - ${game.max_players}`}</li>
+            <li className="list-group-item">{`Play Time: ${game.min_play_time} - ${game.max_play_time} min`}</li>
           </ul>
-          <p className='card-text'>{game.description.substring(0, 150)}</p>
+          <p className="card-text">{game.description.substring(0, 150)}</p>
           <a
-            href='#'
-            className='btn btn-primary'
-            data-toggle='modal'
-            data-target='#gameDetailsModal'
+            href="#"
+            className="btn btn-primary"
+            data-toggle="modal"
+            data-target="#gameDetailsModal"
             id={`${game.id}`}
           >
             Details
           </a>
           {canDelete() && (
             <a
-              data-confirm='Are you sure you want to delete this?'
-              className='btn btn-danger game-delete'
-              rel='nofollow'
-              data-method='delete'
+              data-confirm="Are you sure you want to delete this?"
+              className="btn btn-danger game-delete"
+              rel="nofollow"
+              data-method="delete"
               href={`/games?id=${game.id}`}
             >
-              <div className='material-icons delete-icon'>Delete</div>
+              <div className="material-icons delete-icon">Delete</div>
             </a>
           )}
         </div>
