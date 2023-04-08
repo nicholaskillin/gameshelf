b0a04469c81e954d6ae4a9ca626eb6dab81eab11
Nick Killin <nicholaskillin@gmail.com>
2023-04-07 20:05:36 -0400

    Fix webpacker-dev-server

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,7 +2,9 @@ import 'core-js/stable'
 import 'regenerator-runtime/runtime'
 import gameSearch from 'modules/new_game_search'
 import 'modules/friend_search'
+
 // Support component names relative to this directory:
-var componentRequireContext = require.context('components', true)
-var ReactRailsUJS = require('react_ujs')
+const componentRequireContext = require.context('components', true)
+const ReactRailsUJS = require('react_ujs')
+
 ReactRailsUJS.useContext(componentRequireContext)
--- babel.config.js
+++ babel.config.js
@@ -16,10 +16,11 @@ module.exports = function (api) {
       ],
     ].filter(Boolean),
     plugins: [
-      isProductionEnv && ['babel-plugin-transform-react-remove-prop-types',
+      isProductionEnv && [
+        'babel-plugin-transform-react-remove-prop-types',
         {
           removeImport: true,
-        }
+        },
       ],
       process.env.WEBPACK_SERVE && 'react-refresh/babel',
     ].filter(Boolean),
