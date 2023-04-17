0692c320eee759ceb28ccd60a11cb6f624b2e8be
Nick Killin <nicholaskillin@gmail.com>
2023-04-17 08:20:27 -0400

    Cleanup `User`

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
-class User < ApplicationRecord
+# frozen_string_literal: true
 
+class User < ApplicationRecord
   attr_accessor :activation_token, :reset_token, :avatar
   before_save   :downcase_email
   before_create :create_activation_digest
@@ -15,80 +16,72 @@ class User < ApplicationRecord
   has_many :received_friends, through: :received_friendships, source: 'user'
 
   # - Validations
-  validates :name, presence: true, length: {maximum: 50}
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates :email, presence: true, length: {maximum: 255}, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
-  validates :username, presence: true, length: {maximum: 25}, uniqueness: { case_sensitive: false }, format: { with: /\A[a-zA-Z0-9]+\Z/ }
-  validates :password, presence: true, length: {minimum: 6}, allow_nil: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
 
-  def active_friends
-    friends.select{ |friend| friend.friends.include?(self) }
-  end
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :username, presence: true, length: { maximum: 25 }, uniqueness: { case_sensitive: false }, format: { with: /\A[a-zA-Z0-9]+\Z/ }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
-  def pending_friends
-    friends.select{ |friend| !friend.friends.include?(self) }
-  end
-
-  def pending_friend_requests
-    User.select{ |user| user.friends.include?(self) && !self.friends.include?(user) }
-  end
-
-  # Returns the hash digest of the given string
-  def User.digest(string)
+  def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost:cost)
+    BCrypt::Password.create(string, cost: cost)
   end
 
-  # Returns a random token
-  def User.new_token
+  def self.new_token
     SecureRandom.urlsafe_base64
   end
 
-  # Sends activation email.
-  def send_activation_email
-    UserMailer.account_activation(self).deliver_now
+  def activate
+    update_columns(activated: true, activated_at: Time.zone.now)
+  end
+
+  def active_friends
+    friends.select { |friend| friend.friends.include?(self) }
   end
 
   def authenticated?(attribute, token)
     digest = send("#{attribute}_digest")
     return false if digest.nil?
+
     BCrypt::Password.new(digest).is_password?(token)
   end
 
-  # Activates an account
-  def activate
-    update_columns(activated: true, activated_at: Time.zone.now)
-  end
-
-  # Sets the password reset attributes.
   def create_reset_digest
     self.reset_token = User.new_token
     update_attribute(:reset_digest,  User.digest(reset_token))
     update_attribute(:reset_sent_at, Time.zone.now)
   end
 
-  # Sends password reset email.
-  def send_password_reset_email
-    UserMailer.password_reset(self).deliver_now
+  def pending_friends
+    friends.reject { |friend| friend.friends.include?(self) }
   end
 
-  # Returns true if a password reset has expired.
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
   end
 
+  def pending_friend_requests
+    User.select { |user| user.friends.include?(self) && !friends.include?(user) }
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
   private
 
-    # Converts email to all lower case
-    def downcase_email
-      email.downcase!
-    end
+  def create_activation_digest
+    self.activation_token  = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
 
-    # Creates and assigns the activation token and digest.
-    def create_activation_digest
-      self.activation_token  = User.new_token
-      self.activation_digest = User.digest(activation_token)
-    end
-
+  def downcase_email
+    email.downcase!
+  end
 end
