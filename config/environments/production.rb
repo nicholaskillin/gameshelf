a773d3c61240871c29afcfedb0e0ab8a8c9a19f6
Nick Killin <39142975+nicholaskillin@users.noreply.github.com>
2024-05-25 20:41:05 -0400

    Use full domain for host on production (#101)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "game_shelf_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'gameshelf.online' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
