3e7820038d1f23244f8967537a748d660fe46d05
deployer <nicholaskillin@gmail.com>
2026-03-25 11:43:53 +0000

    fix: Sentry issue 7362749454 - Net::OpenTimeout: execution expired (Net::OpenTimeout)

--- app/models/user.rb
+++ app/models/user.rb
@@ -74,7 +74,7 @@ class User < ApplicationRecord
   end
 
   def send_password_reset_email
-    UserMailer.password_reset(self).deliver_now
+    UserMailer.password_reset(self).deliver_later
   end
 
   private
