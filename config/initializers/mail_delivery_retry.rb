09aa0379f89c23543a14f2c3b7ce711b42b71d99
nicholaskillin <nicholaskillin@gmail.com>
2026-03-25 08:58:51 -0400

    Fix sendgrid timeouts

---
+++ config/initializers/mail_delivery_retry.rb
@@ -0,0 +1,9 @@
+Rails.application.config.to_prepare do
+  ActionMailer::MailDeliveryJob.retry_on(
+    Net::OpenTimeout,
+    Net::ReadTimeout,
+    Net::SMTPServerBusy,
+    wait: :polynomially_longer,
+    attempts: 5
+  )
+end
