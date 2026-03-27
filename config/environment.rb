0df111d9716bd29fa47c7c6ed08f36b27c4b2fee
nicholaskillin <nicholaskillin@gmail.com>
2026-03-26 20:16:43 -0400

    Use postal for email

--- config/environment.rb
+++ config/environment.rb
@@ -2,11 +2,11 @@
 require_relative "application"
 
 ActionMailer::Base.smtp_settings = {
-  user_name: ENV['SENDGRID_USERNAME'],
-  password: ENV['SENDGRID_PASSWORD'],
+  user_name: ENV['POSTAL_USERNAME'],
+  password: ENV['POSTAL_PASSWORD'],
   domain: 'gameshelf.online',
-  address: 'smtp.sendgrid.net',
-  port: 587,
+  address: 'postal.nicholaskillin.com',
+  port: 25,#587,
   authentication: :plain,
   enable_starttls_auto: true,
   open_timeout: 15,
